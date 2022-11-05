0b0ea837d6d2ea630e4ad89c2f9453cd7f95e2e1
Pau <paulakunning@gmail.com>
2022-11-05 10:39:25 -0300

    activity model & db relations

--- api/src/models/Activity.js
+++ api/src/models/Activity.js
@@ -2,22 +2,26 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-    // defino el modelo
-    sequelize.define('country', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        difficulty: {
-            type: DataTypes.ENUM,
-            values: [1, 2, 3, 4, 5],
-            allowNull: false
-        },
-        duration: {
-
-        },
-        season: {
-            type: DataTypes.ENUM, 
-            values: ['Summer', 'Autumn', 'Spring', 'Winter']
-        }
-    })}
+  // defino el modelo
+  sequelize.define(
+    "activity",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      difficulty: {
+        type: DataTypes.ENUM('1', '2', '3', '4', '5'),
+        allowNull: false,
+      },
+      duration: {
+        type: DataTypes.ENUM('1', '2', '3', '4', '5'),
+      },
+      season: {
+        type: DataTypes.ENUM,
+        values: ["Summer", "Autumn", "Spring", "Winter"],
+      },
+    },
+    { timestamps: false }
+  );
+};
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -3,40 +3,42 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
-    id: {
-      type: DataTypes.CHAR(3),
-      allowNull: false, 
-      unique: true,
-      primaryKey: true,
+  sequelize.define(
+    "country",
+    {
+      id: {
+        type: DataTypes.CHAR(3),
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      continent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      area: {
+        type: DataTypes.REAL,
+      },
+      population: {
+        type: DataTypes.INTEGER,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    image: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    continent: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    capital: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    subregion: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    area: {
-      type: DataTypes.REAL,
-    },
-    population: {
-      type: DataTypes.INTEGER
-    }
-  }, 
-  {timestamps: false}
+    { timestamps: false }
   );
 };
