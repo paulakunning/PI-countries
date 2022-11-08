14939842c1cdc670dd89ae146e774e32441ab262
Pau <paulakunning@gmail.com>
2022-11-08 11:34:53 -0300

    store configuration

---
+++ client/src/redux/reducer/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    countries: []
+}
+
+function rootReducer(state = initialState, action){
+    switch (action.payload) {
+        case value:
+    
+        default: return state;
+    }
+}
+
+export default rootReducer
