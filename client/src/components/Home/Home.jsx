7925960ce228bbea493fface83513e277a8b417e
Pau <paulakunning@gmail.com>
2022-11-09 17:57:47 -0300

    pagination

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCountries } from "../../redux/actions/actions";
 import Card from "../Card/Card";
+import Pagination from "../Pagination/Pagination";
 
 export default function Home(){
-
     const dispatch = useDispatch()
     const countries = useSelector(state => state.countries)
     const error = useSelector(state => state.error)
+
+    const [ currentPage, setCurrentPage ] = useState(1)
+    const [ countriesPerPage, setCountriesPerPage ] = useState(10)
+    const indexOfLastCountry = currentPage * countriesPerPage
+    const indexOfFirstCountry = indexOfLastCountry - countriesPerPage
+    const currentCountries = currentPage === 1 ? countries.slice(indexOfFirstCountry, 9) : countries.slice(indexOfFirstCountry, indexOfLastCountry)
+
     useEffect(()=>{
         dispatch(getCountries())
     }, [dispatch])
+
+    const pagination = (pagenumber) => {
+        setCurrentPage(pagenumber)
+    }
 
     if(error){
         return (
@@ -22,10 +34,19 @@ export default function Home(){
     } else if (countries.length){
         return (
             <div>
-                <h1>Home</h1>
-                {countries.map(country => {
-                    return (<Card country={country} key={country.id} />)
-                })}
+                <div>
+                    <Pagination 
+                    countriesPerPage={countriesPerPage}
+                    currentPage={currentPage}
+                    countries={countries.length}
+                    pagination={pagination} />
+                </div>
+                <div>
+                    <h1>Home</h1>
+                    {currentCountries.map(country => {
+                        return (<Card country={country} key={country.id} />)
+                    })}
+                </div>
             </div>
         )
     } else {
--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
-export default function Pagination(){
+export default function Pagination({countriesPerPage, countries, pagination, currentPage}){
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(countries / countriesPerPage); i++) {
+        pageNumbers.push(i)
+    }
+    
+    function handlePrevious(){
+       pagination(currentPage-1)
+    }
+
+    function handleNext(){
+        pagination(currentPage+1)
+    }
+
     return (
         <>
             <h1>Pagination</h1>
+            <button disabled={currentPage === 1 ? true : false} onClick={()=>handlePrevious()} > Previous </button>
+            
+           {/*  <button onClick={()=> pagination(currentPage)} > {currentPage}</button> */}
+           {/* <button>{currentPage}</button> */}
+            {pageNumbers && pageNumbers.map( n => (
+                <button key={n} onClick={()=> pagination(n)} > {n} </button>
+            ))}
+            <button disabled={currentPage === pageNumbers.length ? true : false} onClick={()=>handleNext()} > Next </button>
         </>
     )
 }
