8ca7049f473b08b2430a400c0daca6f36152dfba
Pau <paulakunning@gmail.com>
2022-11-23 12:37:38 -0300

    error 404 page added

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import Form from './components/Form/Form';
 import Detail from './components/CardDetail/CardDetail';
 import Landing from './components/Landing/Landing';
+import Page404 from './components/404/Page404';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/countries' exact component={Home} />
         <Route path='/countries/:id' component={Detail} />
         <Route path='/form' component={Form} />
+        <Route exact path='*' component={Page404} />
       </Switch>
     </div>
   );
