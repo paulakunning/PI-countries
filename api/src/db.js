b690f3af475de0f7585b20c3e8b9912773372865
Paula Kunning <paulakunning@gmail.com>
2023-01-17 14:55:27 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY,
+  DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL,
 } = process.env;
 
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
@@ -11,7 +11,7 @@ const {
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 }); */
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
